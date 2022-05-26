55baf364f17e6c536ed27be72d1decd2eb7bb0be
Juan Felipe <jfortiz87911@umanizales.edu.co>
2022-05-25 22:49:47 -0500

    Se hace la creación del menú el cual se va a er utilizazdo con la libreria dom

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav id="menu">
+      <ul>
+        <li><a href="#">Enlace 1</a></li>
+        <li><a href="#">Enlace 2</a>
+          <ul>
+            <li><a href="#">Enlace 2.1</a></li>
+            <li><a href="#">Enlace 2.2</a></li>
+            <li><a href="#">Enlace 2.3</a></li>
+          </ul>
+      </li>
+      <li><a href="#">Enlace 3</a></li>
+      <li><a href="#">Enlace 4</a></li>
+      <li><a href="#">Enlace 5</a></li>
+    </ul>
+    </nav>
   );
 }
 
