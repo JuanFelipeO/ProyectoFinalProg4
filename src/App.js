c78c7cdead3c6961776cd659ec5848333cc65731
Juan Felipe <jfortiz87911@umanizales.edu.co>
2022-05-26 14:13:54 -0500

    Se pone completamente funcional los diferentes componentes del menú de opciones

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import {
   Route,
   Link
 } from "react-router-dom";
-//import {Indexhvfelipe} from './hojasdevida/Hoja_de_vida_Felipe/Indexhvfelipe';
-import {Hola} from './hojasdevida/Hoja_de_vida_Felipe/Hola';
+import { HojaVidaFelipe } from './hojasdevida/Hoja_de_vida_Felipe/HojaVidaFelipe';
+import { HojaVidaMafe } from './hojasdevida/Hoja_de_vida_Mafe/HojavidaMafe';
 
 
 
@@ -23,20 +23,25 @@ function App() {
     <BrowserRouter>
       <nav class="menu">
         <ul>
-        <li>
-            <Link to="/"> Hoja de vida Juan Felipe</Link>
+          <li>
+            <Link to="/"> Inicio</Link>
           </li>
           <li>
-          <Link to="/hojadevidajuanfelipe"> Hoja de vida Maria Fernanda</Link>
+            <Link to="/hojadeviajuanfelipe"> Hoja de vida Juan Felipe</Link>
           </li>
           <li>
-          <Link to="/contactenos"> Contactanos</Link>
+            <Link to="/hojadevidamariafernanda"> Hoja de vida Maria Fernanda</Link>
+          </li>
+          <li>
+            <Link to="/contactenos"> Contactanos</Link>
           </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Chao/>}/>
-        <Route path="/contactenos" element={<Hola/>}/>
+        <Route path="/hojadeviajuanfelipe" element={<HojaVidaFelipe/>}/>
+        <Route path="/hojadevidamariafernanda" element={<HojaVidaMafe/>}/>
+        <Route path="/contactenos" element={<Chao/>}/>
       </Routes>
     </BrowserRouter>
   );
